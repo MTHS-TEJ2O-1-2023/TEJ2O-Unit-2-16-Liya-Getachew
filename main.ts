b04630e6819dcf41b2bf3efc154c2c63528646b9
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-12-06 12:35:58 -0500

    readme

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Liya G.
+ * Created on: Dec 2023
+ * This program does bluetooth
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(1)
+basic.showIcon(IconNames.Rabbit)
