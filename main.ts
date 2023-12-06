3061f741833f629cc76dc4596669ab38026214e7
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-12-06 12:54:02 -0500

    ts vers

--- main.ts
+++ main.ts
@@ -5,6 +5,34 @@
  * This program does bluetooth
 */
 
+// variables
+let distanceToObject: number = 0
+
 // setup
 radio.setGroup(1)
 basic.showIcon(IconNames.Rabbit)
+
+while (true) {
+  if (input.buttonIsPressed(Button.A) === true) {
+    // find distance from sonar
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+      DigitalPin.P1,
+      DigitalPin.P2,
+      PingUnit.Centimeters
+    )
+
+    if (distanceToObject < 10) {
+      radio.sendString('Too Close')
+    } else {
+      radio.sendString('Safe zone')
+    }
+  }
+
+  // recieve from other microbit
+  radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+  })
+}
